0cf20268db7af5d41ae8454ca735e060f1a358ea
fnagel <felixnagel@users.noreply.github.com>
2016-06-24 12:58:38 +0200

    Add files via upload

--- src/BlackBox.php
+++ src/BlackBox.php
@@ -3,35 +3,30 @@
 namespace LuckyNail\Simple;
 
 class BlackBox{
-	protected static $_aTrunk = [];
+	protected static $_aBox = [];
 	protected $_sKey;
-
 	public function __construct($sKey){
+		self::_ensure_existence($sKey);
 		$this->_sKey = $sKey;
 	}
-	private static function _ensure_existing_key($sKey){
-		if(!isset(self::$_aTrunk[$sKey])){
-			self::$_aTrunk[$sKey] = [];
+	private static function _ensure_existence($sKey){
+		if(!isset(self::$_aBox[$sKey])){
+			self::$_aBox[$sKey] = [];
 		}
 	}
-	public static function put($sKey, $mVal){
-		self::_ensure_existing_key($sKey);
-		self::$_aTrunk[$sKey][] = $mVal;
+	public static function global_insert($sKey, $mVal){
+		self::_ensure_existence($sKey);
+		self::$_aBox[$sKey][] = $mVal;
 	}
-	public static function lay_under($sKey, $mVal){
-		self::_ensure_existing_key($sKey);
-		array_unshift(self::$_aTrunk[$sKey], $mVal);
+	public function insert($mVal){
+		self::global_insert($this->_sKey, $mVal);
 	}
-	public function look(){
-		if(!isset(self::$_aTrunk[$this->_sKey])){
-			return [];
-		}
-		return self::$_aTrunk[$this->_sKey];
+	public function get(){
+		return self::$_aBox[$this->_sKey];
 	}
-	public function take(){
-		$mResult = self::look($this->_sKey);
-		unset(self::$_aTrunk[$this->_sKey]);
+	public function truncate(){
+		$mResult = $this->get();
+		unset(self::$_aBox[$this->_sKey]);
 		return $mResult;
 	}
-
 }
